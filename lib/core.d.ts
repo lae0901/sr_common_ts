4efcb06cd4be6c428da5ec623a436bfc3a13d965
lae0901 <stephenrichter@gmail.com>
2020-01-18 11:41:59 -0500

    core.ts type declarations

---
+++ lib/core.d.ts
@@ -0,0 +1,24 @@
+import { rxp } from './regex_core';
+export { rxp };
+export declare function lines_findFirst(lines: string[], findText: string, options?: {
+    start?: number;
+}): {
+    linn: number;
+    coln: number;
+};
+export declare function object_toQueryString(obj: {}): string;
+export declare function path_removeQueryString(str: string): string;
+export declare function path_toFileUri(path: string): string;
+export declare function scan_charNeAll(text: string, bx: number, pattern: string): number;
+export declare function scan_revCharEqAny(text: string, bx: number, anyChar: string): number;
+export declare function scan_revCharNeAll(text: string, bx: number, pattern: string): number;
+export declare function scan_revSepWord(text: string, pos: number, wsChars: string): {
+    text: string;
+    bx: number;
+} | null;
+export declare function string_contains(str: string, pattern: string): boolean;
+export declare function string_dequote(text: string): string;
+export declare function string_rtrim(str: string): string;
+export declare function string_startsWith(text: string, startText: string): boolean;
+export declare function string_tail(str: string, num: number): string;
+export declare function string_wordBx(text: string, word: string, ix: number): number;
