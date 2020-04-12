870495b8bfcd871683dee9a36738010f4217a1c3
lae0901 <stephenrichter@gmail.com>
2020-04-12 11:05:38 -0400

    regexPattern_toFragments

--- src/regex_frag.js
+++ src/regex_frag.js
@@ -227,4 +227,204 @@ function regexPattern_toFragments(pattern)
   return fragArray;
 }
 
+// site/js/regex_core.js
+// date: 2019-09-14
+// desc: regex functions and constants. Used to enhance functionality of javascript
+//       built in regex features.
+
+// rxp - const object that contains regex match patterns.
+const rxp = {
+  any: '\\.',       // match any char
+  zeroMoreWhitespace: `\\s*`,
+  singleQuoteQuoted: `\\s*'(?:\\\\.|[^'\\\\])*'`,
+  doubleQuoteQuoted: `\\s*"(?:\\\\.|[^"\\\\])*"`,
+  jsonNameVluSep: `\\s*:`,
+  beginString: `^\\s*`,
+  jsonStart: `\\s*{`,
+  jsonEnd: `\\s*}`,
+  jsonStartArray: `\\s*\\[`,
+  jsonStartObject: `\\s*\\{`,
+  comma: `\\s*,`,
+  or: '|',
+  beginCapture: '(',
+  closeParen: '\\)',
+  comment: '\\/\\*.+?\\*\\/|\\/\\/.*(?=[\\n\\r])',
+  endCapture: ')',
+  endCaptureZeroOne: ')?',
+  endCaptureZeroMore: ')*',
+  endCaptureOneMore: ')+',
+  oneMoreNumeric: '[\\d.]+',
+  oneMoreDigits: '\\d+',
+  oneMoreAlpha: '[A-Za-z]+',
+  oneMoreWord: '\\w+',
+  oneMoreWhitespace: '\\s+',
+  openParen: '\\(',
+  stringStart: '^',
+  stringEnd: '$',
+  variableName: `[a-zA-Z_]\\w*`,
+  zeroOneAny: '\\.?',
+  zeroMoreWord: '\\w*',
+  zeroMoreWhitespace: '\\s*',
+
+  jsonVluStart: function ()
+  {
+    return this.zeroMoreWhitespace + this.beginCapture + this.singleQuotedString +
+      this.or + this.varName + this.or + this.jsonStartArray +
+      this.or + this.jsonStartObject + this.endCapture
+  },
+  jsonPropName: function ()
+  {
+    return this.zeroMoreWhitespace + this.beginCapture + this.singleQuotedString +
+      this.or + this.varName + this.endCapture
+  },
+  jsonNameVluPair: function ()
+  {
+    return this.zeroMoreWhitespace + this.beginCapture + this.singleQuotedString +
+      this.or + this.varName + this.endCapture +
+      this.jsonNameVluSep +
+      this.beginCapture + this.singleQuotedString +
+      this.or + this.varName + this.endCapture
+  },
+  beginNamedCapture: function (name)
+  {
+    return `(?<${name}>`;
+  },
+  escape: function (char) { return '\\' + char }
+}
+
+// -------------------------- regex_exec -----------------------------------
+// match to a pattern, starting at bx in text string.
+// re_pattern:  either a RegExp object or regular expression pattern.
+// map_capture:  map captured matches. [{ix, name, trim:true, fxName }]
+//               map from array of captured matches to properties in return value.
+//               ix: index in capture array of value to map
+//               name: property name to map to in the return object.
+//               trim: when true, trim whitespace from capture value when mapping
+//                     to map to property name in return object.
+//               fxName: property name in return object in which to store the
+//                       found position in the search text of the trimmed capture 
+//                       value.
+// const rv = regex_exec(stmt, bx, rxx_dataDefn, [{ ix: 1, name: 'const' },
+// { ix: 2, name: 'datatype' }, { ix: 3, name: 'pointer' }]);
+function regex_exec(text, bx, re_pattern, map_capture)
+{
+  let matchBx = -1;
+  let matchLx = 0;
+  let matchOx = -1;
+  let matchText = '';
+  let capture_ix = bx;
+
+  // setup the regular expression to execute.
+  let re = re_pattern;
+  if (typeof (re_pattern) == 'string')
+  {
+    re = new RegExp(re_pattern, 'g');
+  }
+
+  // start position in text
+  re.lastIndex = bx;
+
+  const reg_rv = re.exec(text);
+
+  if (reg_rv != null)
+  {
+    matchBx = reg_rv.index;
+    matchOx = matchBx - bx;
+    matchText = reg_rv[0];
+    matchLx = matchText.length;
+  }
+
+  let rv = { matchBx, matchLx, matchOx, matchText, execRv: reg_rv };
+
+  // map from capture array to properties in return value.
+  if (map_capture)
+  {
+    for (let mx = 0; mx < map_capture.length; ++mx)
+    {
+      const item = map_capture[mx];
+      if (item.ix < reg_rv.length)
+      {
+        let capture_text = reg_rv[item.ix];
+        rv[item.name] = capture_text;
+
+        // trim blanks from the capture variable.
+        if (item.trim)
+        {
+          if (!capture_text)
+            capture_text = '';
+          else
+            capture_text = string_trim(capture_text);
+          rv[item.name] = capture_text;
+        }
+
+        // the found position of the capture value. Scan the input text for the
+        // capture text. Store the found pos in the specified propert of the return
+        // object.
+        if (item.fxName)
+        {
+          const fx = text.indexOf(capture_text, capture_ix);
+          rv[item.fxName] = fx;
+
+          // next time look for capture text, start looking after the location of
+          // this just found capture text.
+          capture_ix = fx + capture_text.length;
+        }
+      }
+    }
+  }
+
+  return rv;
+}
+
+// ------------------------- string_trim --------------------
+function string_trim(str)
+{
+  if (typeof str == 'number')
+    str = str.toString();
+  if (!str)
+    return str;
+  else
+  {
+    let s1 = str.replace(/(\s+$)|(^\s+)/g, "");
+    return s1;
+  }
+}
+
+// ---------------------------- string_substrLenient --------------------
+// return substring of the input string. only, clip the results if start or end
+// pos are out of bounds of the string.
+function string_substrLenient(str, fx, lx = -1)
+{
+  if ((typeof str) != 'string')
+    return '';
+
+  // move from from negative to zero. Reduce length by the adjusted amount.
+  if (fx < 0)
+  {
+    var adj = 0 - fx;
+    fx += adj;
+    if (lx != -1)
+    {
+      lx -= adj;
+      if (lx < 0)
+        lx = 0;
+    }
+  }
+
+  if (fx >= str.length)
+    return '';
+  if (lx == -1)
+    return str.substr(fx);
+
+  // remaining length.
+  var remLx = str.length - fx;
+
+  // trim length if remaining lgth exceeded.
+  if (lx > remLx)
+    lx = remLx;
+
+  return str.substr(fx, lx);
+}
+
+
 export { regexPattern_toFragments } ;
